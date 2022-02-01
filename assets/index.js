b02aaac7a16aacec7977d45750de2a8d50bff03c
Ben Gomez <3gobenjamin@gmail.com>
2022-02-01 17:27:04 -0600

    cleaned up and definite progress

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,5 @@
 var start = document.querySelector('#start');
 var timerEl = document.getElementById('timer');
-
 var qContainer = document.getElementById('questionsContainer');
 var questionEl = document.getElementById('question');
 var answerA = document.getElementById('A');
@@ -9,8 +8,8 @@ var answerC = document.getElementById('C');
 var answerD = document.getElementById('D');
 var options =document.getElementById('answers') ;
 var index = 0;
-
-
+var timeLeft = 30;
+var randomizeQuestions, currentQuestionIndex
 
 var questions= [
     {
@@ -22,11 +21,17 @@ var questions= [
         correct: "C",
     }, {
         question: "Function (_____) are the values received by the function when it is invoked(called)?",
-        options: { A: "A) variables", B: "B) arrays", C: "C) booleans", D: "D) arguments", },
+        answerA: "A) variables",
+        answerB: "B) arrays",
+        answerC: "C) booleans",
+        answerD: "D) arguments",
         correct: "D",
     }, {
         question: "JSON is an acronym that stands for:",
-        options: { A: "A) Java Subject Oriented Node" , B: "B) JavaScript Object Notation" , C: "C) Java Sequenced Object Notation" , D: "D) Java Script Oriented Node" , },
+        answerA: "A) Java Subject Oriented Node",
+        answerB: "B) JavaScript Object Notation",
+        answerC: "C) Java Sequenced Object Notation",
+        answerD: "D) Java Script Oriented Node",
         correct: "B",
     }, {
         question: "When variables or values are equal in value and type we use this operator to represent that:",
@@ -38,10 +43,17 @@ var questions= [
     }
 ];
 
+    
 
-document.getElementById("start").addEventListener("click",function(){
-    var timeLeft = 5;
+function startQuiz () {
+    questionShows()
+    timerStarts()
+}
+
+function timerStarts() {
     var timeInterval = setInterval(function(){
+        qContainer.classList.remove('hide');
+        
         if (timeLeft > 1) {
             timerEl.textContent = timeLeft + ' seconds remaining';
             timeLeft--;
@@ -51,110 +63,61 @@ document.getElementById("start").addEventListener("click",function(){
         } else {
             timerEl.textContent = "Let's see how you did!";
             clearInterval(timeInterval);
+            endQuiz()
+        }
+
+        if(index >= questions.length) {
+            clearInterval(timeInterval)
+            endQuiz()
         }
     }, 1000);
- 
-});
+}
 
-var questionsIndex = 0;
-
-var qAndAsShown = 
- document.getElementById("start").addEventListener("click",function(){
+function questionShows() {
     document.getElementById('question').innerHTML = questions[index].question;
     document.getElementById('A').innerHTML = questions[index].answerA;
     document.getElementById('B').innerHTML = questions[index].answerB;
     document.getElementById('C').innerHTML = questions[index].answerC;
     document.getElementById('D').innerHTML = questions[index].answerD;
-});
+}
+
+
 
 var userChoice = document.getElementById("answers");
 var answersList = document.querySelectorAll("#A, #B, #C, #D");
-var answersArray = [...answersList]
+var answersArray = [...answersList];
+var correctAnswer1 = questions[0].correct;
+var question1 = questions[0];
+var state = questions.answersList;
 
-userChoice.addEventListener("click", function(event){
-    var element = event.target;
 
-    if (element.matches(".answer")){
-        var state = questions.answersList;
-        
-        if (state == questions.correct) {
-            console.log('correct answer')
-        } 
+function checkAnswerAndGoForth (event) {
+    var pickedChoice = event.target.id
+    if(pickedChoice == questions[index].correct) {
+        console.log("correct")
+    }else {
+        console.log("incorrect")
+        timeLeft -= 2 ;
     }
-});
-
-
-
-// function showQuestions() {
-//     if (index >= questions.length) 
-//     return;
-//     var show =document.getElementById("question");
-//     var qAdd = questions[index];
-//     show.innerHTML = qAdd.answers;
-//     showQuestions.innerHTML= qAdd;
-//     Object.entries(qAdd.options).forEach(([letter,text])=>{
-//         var answers = document.getElementById(letter);
-//         answers.innerHTML = text
-//         answers.dataset.correct = qAdd.correct === letter;
-//     })
-//     index++;
-// }
-
-// showQuestions();
-
-//this will check answers
-// document.getElementById("answers").addEventListener("click", function(e) {
-//     const tgt = e.target;
-//     if (tgt.type && tgt.type === "button") {
-//       document.getElementById("result").innerText = tgt.dataset.correct === "true" ? "Correct" : "Incorrect"
-//     }
-//   })
-
-// var quizClicks = document.getElementById('A','B','C','D');
-
-// quizClicks.addEventListener('click',function(event){
-//     var element = event.target;
-//     if (element.matches(".answer")){
-
-//     }
-// })
-                
-
-
-
-
-// function clickable(){
-//     var correctChoice = questions[index].correct;
     
-// }
+    index++;
+    if(index < questions.length) {
+        questionShows()
+    }
+}
+
+function endQuiz() {
+    alert("yo, we're done here")
+}
+
+document.getElementById("start").addEventListener("click", startQuiz);
+document.getElementById('A').addEventListener("click", checkAnswerAndGoForth)
+document.getElementById('B').addEventListener("click", checkAnswerAndGoForth)
+document.getElementById('C').addEventListener("click", checkAnswerAndGoForth)
+document.getElementById('D').addEventListener("click", checkAnswerAndGoForth)
 
 
 
-// var start = document.getElementById('start');
-//  start.addEventListener("click",function () {
-//         console.log('clicked');
-// });
-
-
-// function startQuiz() {
-//     if (){}
-// }
-
-
-// startTimer();
-// start.addEventListener("click", questionsCalled);
-
-//from line 43: // function startTimer() {
-
-
-
-// startButton();
-// function startButton(){
-//     document.querySelector("startButton");{   
-// }
-// for (var i = 0; i < questions.length;i++){
-//     questions[i].setAttribute("style", "color:green; font-size: 22px")
-// }
 
 
 
